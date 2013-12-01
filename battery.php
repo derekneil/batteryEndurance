71043a0a8eecb7f9ee77cbb8e1d6926fa0fddf6c
Derek Neil <derekrneil@gmail.com>
2013-12-01 12:54:18 -0400

    initial working commit

---
+++ battery.php
@@ -0,0 +1,1 @@
+/Users/derekneil/BluenoseWeb/battery.php
---
+++ battery.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#track the battery life of macbookpro over time
+
+#schedule this script as a cron job twice a day
+#aim for when the computer will be on, and you'll have internet
+#cron 7:45am
+# 45 7 * * * ~/.battery.sh
+
+#cron 6:30pm
+# 30 18 * * * ~/.battery.sh
+
+#check for write permissions in current dir
+#if ...
+#return 1;
+
+#collect battery info and store in tmp file
+#you have to prune the data from ioreg
+#    | |           "MaxCapacity" = 8667
+#    | |           "CycleCount" = 5
+#    | |           "DesignCapacity" = 8440
+#    | |           "DesignCycleCount9C" = 1000
+#it was just the first hit on google
+# TODO: there might be a faster way to get this info
+entry=`date +"20%y,%m,%d,%H,%M"`
+entry=${entry}\,`/usr/sbin/ioreg -w0 -l | grep -E '\"MaxCapacity\"|\"DesignCapacity\"'  | cut -d " " -f19 | tr '\r\n' ',' | rev | cut -c 2- | rev`
+entry=${entry}\,`/usr/sbin/ioreg -w0 -l | grep -E '\"CycleCount\"|\"DesignCycleCount'  | cut -d " " -f19 | tr '\r\n' ',' | rev | cut -c 2- | rev`
+
+
+#save to file for batch processing if you haven't been online in a while
+echo $entry >> .batteryHistoryToUpload
+
+#try to upload file contents to api
+baseURL='http://web.cs.dal.ca/~dneil/battery.php'
+while read line           
+do           
+    url=${baseURL}\?entry=$line
+    curl -s ${url} > /dev/null
+
+    #unsuccessfull, save back to another file
+    if [ ${?} == 1 ]; then
+    	echo $line >> .batteryHistoryNotUploaded
+    fi
+
+done < .batteryHistoryToUpload
+
+`rm .batteryHistoryToUpload`
+`touch .batteryHistoryToUpload`
+
+remaining=".batteryHistoryNotUploaded"
+if [ -e "$remaining" ]; then #if .batteryHistoryNotUploaded file exists
+	`mv .batteryHistoryNotUploaded .batteryHistoryToUpload`
+fi
+
+#make sure permissions are set correctly for file
+`chmod 644 .batteryHistoryToUpload`
+
+#check out your sweet graph on the server you put battery.php and the highcharts framework on!
